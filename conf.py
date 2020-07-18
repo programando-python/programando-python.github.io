a0dd116809599e212aa525816d181e34639e8ff8
Nahuel Tori <curso.programando.python@gmail.com>
2020-07-17 23:03:04 -0300

    Nikola auto commit.
    Nikola version: 8.1.1

--- conf.py
+++ conf.py
@@ -20,7 +20,7 @@ BLOG_AUTHOR = "Nahuel Tori"  # (translatable)
 BLOG_TITLE = "Programando Python"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
-SITE_URL = "https://example.com/"
+SITE_URL = "https://programando-python.github.io/"
 # This is the URL where Nikola's output will be deployed.
 # If not set, defaults to SITE_URL
 # BASE_URL = "https://example.com/"
