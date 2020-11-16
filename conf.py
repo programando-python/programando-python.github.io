9672b874720e4d610f19f516ea1aa81f130b021c
Nahuel Tori <curso.programando.python@gmail.com>
2020-11-16 17:54:49 -0300

    Disqus config

--- conf.py
+++ conf.py
@@ -979,7 +979,7 @@ COMMENT_SYSTEM = "disqus"
 # depends on what comment system you use. The default is
 # "nikolademo" which is a test account for Disqus. More information
 # is in the manual.
-COMMENT_SYSTEM_ID = ""
+COMMENT_SYSTEM_ID = "programando-python"
 
 # Create index.html for page folders?
 # WARNING: if a page would conflict with the index file (usually
